95b920e2f4ceb8d93c3485d9975aeb58f6c44264
Alena.Lialiushkina <pluto_16@mail.ru>
2025-08-05 17:48:15 +1000

    feat: добавляет helmet, фавикон, метаинфу

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -13,23 +14,56 @@ import CookieConsent from "./components/CookieConsent";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/authors" element={<Authors />} />
-        <Route path="/protected-objects" element={<ProtectedObjects />} />
-        <Route
-          path="/development-regulations"
-          element={<DevelopmentRegulations />}
+    <>
+      <Helmet>
+        <title>
+          Достопримечательное место "Исторический центр города Владивостока"
+        </title>
+        <meta
+          name="description"
+          content="Официальный сайт Достопримечательного места 'Исторический центр города Владивостока'. Карты, объекты, история, охрана."
         />
-        <Route path="/politics" element={<Politics />} />
-        <Route path="/cookies" element={<Cookies />} />
-      </Routes>
-      <Footer />
-      <CookieConsent />
-    </BrowserRouter>
+        <meta
+          name="keywords"
+          content="Владивосток, культурное наследие, история, объекты, карта, охрана, памятники"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://historycenter-vl.ru" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content="Исторический центр Владивостока" />
+        <meta
+          property="og:description"
+          content="Официальный сайт Достопримечательного места 'Исторический центр города Владивостока'."
+        />
+        <meta
+          property="og:image"
+          content="https://historycenter-vl.ru/og-image.jpg"
+        />
+        <meta property="og:url" content="https://historycenter-vl.ru" />
+        <meta property="og:type" content="website" />
+      </Helmet>
+
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/authors" element={<Authors />} />
+          <Route path="/protected-objects" element={<ProtectedObjects />} />
+          <Route
+            path="/development-regulations"
+            element={<DevelopmentRegulations />}
+          />
+          <Route path="/politics" element={<Politics />} />
+          <Route path="/cookies" element={<Cookies />} />
+        </Routes>
+        <Footer />
+        <CookieConsent />
+      </BrowserRouter>
+    </>
   );
 }
 
